bdbaa742fade8aa62a156d84697941083aca9715
Marcos-ArPo <marcos.arbin.pozo.al@iespoligonosur.org>
2025-11-18 16:28:59 +0100

    Clases creadas

---
+++ Parking/src/main/java/con/conector.java
@@ -0,0 +1,13 @@
+package con;
+
+import java.sql.*;
+
+public class conector {
+    private String url = "jdbc:mysql://localhost:3306/parking";
+    private String usr = "root";
+    private String pwd = "1234";
+    
+    public Connection con() throws Exception{
+        return DriverManager.getConnection(url, usr, pwd);
+    }
+}
